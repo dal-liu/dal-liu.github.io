53c6bb7825a44ebeeeff121d926d4983e5968de2
Daniel <daniel.albert.liu@gmail.com>
2024-01-04 01:30:12 -0600

    Added a navbar

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+var navbar = document.querySelector('.navbar');
+var aboutPage = document.querySelector('#about');
+var projectsPage = document.querySelector('#projects');
+
+document.querySelectorAll('.nav-item').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    switch (item.id) {
+      case 'nav-home':
+        var top = 0;
+        break;
+      case 'nav-about':
+        if (!aboutPage) {
+          return;
+        }
+        var top = aboutPage.offsetTop - 50;
+        break;
+      case 'nav-projects':
+        if (!projectsPage) {
+          return;
+        }
+        var top = projectsPage.offsetTop - 50;
+        break;
+      default:
+        return;
+    }
+
+    window.scrollTo({
+      top,
+      behavior: 'smooth',
+    });
+    console.log('clicked');
+  });
+});
+
+document.querySelector('.arrow-down').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (!aboutPage) {
+    return;
+  }
+
+  window.scrollTo({
+    top: aboutPage.offsetTop - 50,
+    behavior: 'smooth',
+  });
+});
+
+window.onscroll = () => {
+  if (!navbar) {
+    return;
+  }
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+};
